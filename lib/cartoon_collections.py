4eb687c787d2a039011f0c37191a6f1a5fbf4f7b
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-10-23 15:30:24 -0400

    pass tests

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,19 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(list):
+    for name in list:
+        print(f"{list.index(name) + 1}. {name}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(list):
+    return [element.capitalize() + "!" for element in list]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(list):
+    for word in list:
+        if len(word) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(list):
+    for element in list:
+        if element == 'cheddar' or element == 'gouda' or element == 'camebert':
+            return element
+    
+    return None
